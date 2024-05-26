05ea86162c3ef4421ad9137be6318f3dd4aa337a
scfulga <146138900+scfulga@users.noreply.github.com>
2024-05-26 16:22:37 +0300

    First function done

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log("Hello world!1")
+let choices = ["Rock","Paper", "Scissors"] 
+function getComputerChoice(){
+    return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function getHumanChoice(){
+    let input = prompt("Choose something");
+    return input;
+}
+
+console.log(getHumanChoice())
+
