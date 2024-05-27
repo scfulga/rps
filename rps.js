b4a33186f8426b89f8eaee8de74d8e32ce5ee2f5
scfulga <146138900+scfulga@users.noreply.github.com>
2024-05-27 22:47:25 +0300

    Wrote the logic to the human choice
    Declared the players score variables
    
    Wrote the logic for a single round
    
    Wrote the logic for the entire game

--- rps.js
+++ rps.js
@@ -1,12 +1,53 @@
-let choices = ["Rock","Paper", "Scissors"] 
+let choices = ["rock", "paper", "scissors"]
+
 function getComputerChoice(){
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
 function getHumanChoice(){
-    let input = prompt("Choose something");
-    return input;
+    let input = prompt("Choose Rock, Paper or Scissors ");
+    if (choices.includes(input.toLowerCase())){
+        return input.toLowerCase();
+    }
+    else {
+        while(!(choices.includes(input.toLowerCase()))){
+
+            alert("not valid, try again");
+            input = prompt("Choose Rock , Paper or Scissors ");
+
+        } 
+    }
 }
 
-console.log(getHumanChoice())
+
+function playGame(){
+    let humanScore = 0;
+    let computerScore = 0;
+    function playRound(humanChoice, computerChoice) {
+        if (humanChoice === computerChoice){
+            console.log("It's a tie!")
+        }
+        else if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock"){
+            console.log("You won!");
+            humanScore++;
+        }
+        else{
+            console.log("Computer won!");
+            computerScore++;
+        }
+}
+    
+    for(i = 0; i < 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+        console.log(`You chose: ${humanSelection}`);
+        console.log(`Computer chose: ${computerSelection}`);
+        console.log(`Score ${humanScore} : ${computerScore}`);
+    }
+    
+
+}
+
+playGame();
 
