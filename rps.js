7c9c2d38ea856b7255d32c9e6f9513856bf3ff5b
scfulga <146138900+scfulga@users.noreply.github.com>
2024-06-28 15:01:13 +0300

    Replaced all of the 'console.log's into DOM methods to display the score and the current state of the game, removed the getHumanChoice because it's useless now

--- rps.js
+++ rps.js
@@ -1,7 +1,14 @@
-const choices = ['rock', 'paper', 'scissors']
+
+let computerScore = 0;
+let humanScore = 0;
+
+const choices = ['rock', 'paper', 'scissors'];
+
 const rockBtn = document.querySelector("#rock");
 const paperBtn = document.querySelector("#paper");
 const scissorsBtn = document.querySelector("#scissors");
+const score = document.querySelector("#score");
+const gameResult = document.querySelector("#result");
 
 rockBtn.addEventListener('click', () => {
     playRound('rock', getComputerChoice());
@@ -15,43 +22,25 @@ scissorsBtn.addEventListener('click', () => {
     playRound('scissors', getComputerChoice());
 });
 
-
-
-
-
-function getComputerChoice(){
+function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)];
 }
 
-function getHumanChoice(){
-    let input = prompt("Choose Rock, Paper or Scissors ");
-    if (choices.includes(input.toLowerCase())){
-        return input.toLowerCase();
-    }
-else {
-        while(!(choices.includes(input.toLowerCase()))){
-
-            alert("not valid, try again");
-            input = prompt("Choose Rock, Paper or Scissors ");
-
-        } 
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        gameResult.innerHTML = "It's a tie!";
+    } else if (
+        (humanChoice === "rock" && computerChoice === "scissors") || 
+        (humanChoice === "scissors" && computerChoice === "paper") || 
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        humanScore++;
+        gameResult.innerHTML = "You won!";
+        score.innerHTML = `${humanScore} - ${computerScore}`;
+    } else {
+        computerScore++;
+        gameResult.innerHTML = "Computer won!";
+        score.innerHTML = `${humanScore} - ${computerScore}`;
     }
 }
 
-    let humanScore = 0;
-    let computerScore = 0;
-    function playRound(humanChoice, computerChoice) {
-        if (humanChoice === computerChoice){
-            console.log("It's a tie!")
-        }
-        else if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock"){
-            console.log("You won!");
-            humanScore++;
-        }
-        else{
-            console.log("Computer won!");
-            computerScore++;
-        }
-
-
-    }   
